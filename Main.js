f8ee5a6515702c62572a558996878468965eb235
ArshiMahajan22 <77196068+ArshiMahajan22@users.noreply.github.com>
2021-08-12 12:34:11 +0530

    Update Main.js

--- Main.js
+++ Main.js
@@ -8,7 +8,7 @@ function preload(){
 
 function setup(){
     canvas = createCanvas(300, 300);
-    canvas.position(490, 150);
+    canvas.center();
     video = createCapture(VIDEO);
     video.hide();
     video.size(300, 300);
